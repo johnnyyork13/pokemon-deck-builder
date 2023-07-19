b934bb3568142752f45a27935ecbcb9d074a9d7a
johnnyyork13 <johnnyyork13@hotmail.com>
2023-07-19 10:27:08 -0400

    add test functionality to see how API works

---
+++ index.js
@@ -0,0 +1,26 @@
+//28a09ffa-8ba8-4297-b7b3-e9be96bc4508
+//https://api.pokemontcg.io/v2/cards
+
+const pokeImg = document.getElementById('pokeImg');
+const submitBtn = document.getElementById('submitBtn');
+const textBox = document.getElementById('textBox');
+
+async function getAllData() {
+    const allData = "https://api.pokemontcg.io/v2/sets";
+    const res = await fetch(allData);
+    const data = await res.json();
+    console.log(data);
+}
+
+async function loadCard(id) {
+    const url = `https://api.pokemontcg.io/v2/cards/swsh8-${id}`;
+    const res = await fetch(url);
+    const data = await res.json();
+    pokeImg.src = data.data.images.small;
+}
+
+getAllData();
+
+submitBtn.addEventListener('click', function() {
+    loadCard(textBox.value);
+})
